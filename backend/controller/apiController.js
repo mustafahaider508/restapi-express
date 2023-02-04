944d32d60d3da94a83cb2589a6674271f87a856c
Mustafa Haider <42119893+mustafahaider508@users.noreply.github.com>
2023-02-04 11:45:55 +0500

    CRUD API

--- backend/controller/apiController.js
+++ backend/controller/apiController.js
@@ -1,20 +1,43 @@
-const getApi = async(req,res) => {
-    res.status(200).json({ message: "get api" });
+const asyncHander = require("express-async-handler");
+const Goal = require("../models/goalModel")
 
-}
+const getApi = asyncHander(async(req,res) => {
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 
-const postApi = async (req,res) => {
+})
+
+const postApi = asyncHander(async (req,res) => {
+    console.log(req.body)
   if(!req.body.text){
       res.status(400)
       throw new Error("Please add a field");
   }
-    res.status(200).json({message:"post api"})
-}
+   const createGoals = await Goal.create({
+       text:req.body.text
+   })
+    res.status(200).json(createGoals);
+})
 
-const putApi = async (req,res) => {
-    res.status(200).json({ message: `put req on id ${req.params.id}` });
-}
-const delApi = async (req,res) => {
-      res.status(200).json({ message: `delete req on id ${req.params.id}` });
-}
+const putApi = asyncHander(async (req,res) => {
+    const gaol = Goal.findById(req.params.id);
+    if (!gaol) {
+      res.status(400);
+      throw new Error("Goal not found");
+    }
+    const updateGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{
+        new:true
+    })
+
+      res.status(200).json(updateGoal);
+})
+const delApi = asyncHander(async (req,res) => {
+     const gaol = Goal.findById(req.params.id);
+     if (!gaol) {
+       res.status(400);
+       throw new Error("Goal not found");
+     }
+    const deletegoal = await Goal.remove();
+      res.status(200).json("goal deleted");
+})
  module.exports = { getApi, postApi, putApi, delApi };
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 const port = process.env.PORT || 5000;
 const {errorHandeler} = require("./errorMiddleware/errorMiddleware");
-
+dotenv.config({ path: "./config.env" });
+require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use("/api", require("./router/api"));
-app.use(errorHandeler);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
 });
+app.use("/api", require("./router/api"));
+app.use(errorHandeler );
+
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
